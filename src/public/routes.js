5f4ae77ae0c1a402b080e7140341c518179e8658
Jesús Rogliero <84638984+jesusrogliero@users.noreply.github.com>
2023-06-14 16:41:15 -0400

    removidos archivos innecesarios

--- src/public/routes.js
+++ src/public/routes.js
@@ -2,15 +2,6 @@
 
 let routes = [
     {path: "/", component: () => import("./components/home.js") },
-    {path: "/taxes", component: () => import("./components/taxes.js") },
-    {path: "/clients", component: () => import("./components/clients.js") },
-    {path: "/currencies", component: () => import("./components/currencies.js") },
-    {path: "/products", component: () => import("./components/products.js") },
-    {path: "/prices", component: () => import("./components/prices.js") },
-    {path: "/purchases_orders", component: () => import("./components/purchasesOrders.js") },
-    {path: "/providers", component: () => import("./components/providers.js") },
-    {path: "/costs", component: () => import("./components/costs.js") },
-    {path: "/invoices", component: () => import("./components/invoices.js") },
 
 ];
 
