b5dfa7f6fd026161369ac7c0bf0842987ada0dd4
Jesús Rogliero <84638984+jesusrogliero@users.noreply.github.com>
2023-06-16 09:43:16 -0400

    codigo innecesario retirado

--- src/connection.js
+++ src/connection.js
@@ -11,74 +11,36 @@ const sequelize = new Sequelize({
 });
 */
 
-const sequelize = new Sequelize('sqlite::memory:', {logging:false});
+const sequelize = new Sequelize('sqlite::memory:', { logging: true });
 
 
-
-const seeds =  async function(Model, data) {
+/*
+const seeds = async function (Model, data) {
 	try {
 		for (let i = 0; i < data.length; i++) {
 			await Model.findOrCreate({
 				where: data[i]
 			});
 		}
-		
+
 	} catch (error) {
 		console.log(error);
 	}
-	
-  };
 
-
+};
+*/
 
 (async () => {
 	await sequelize.sync();
 
-
-	await seeds(require('./models/Currency.js'), [
-		{
-			name: 'Bolivares',
-			symbol: 'Bs',
-			exchange_rate: 1
-		},
-		{
-			name: 'Dolar',
-			symbol: '$',
-		},
-
+	/*
+	await seeds(require('./models/PurchaseOrderState.js'), [
+	{state: 'Pendiente'},
+	{state: 'Generada'},
+	{state: 'Aprobada'},
 	]).then(e => console.log);
-
-
-  	await seeds(require('./models/PurchaseOrderState.js'), [
-		{state: 'Pendiente'},
-		{state: 'Generada'},
-		{state: 'Aprobada'},
-	]).then(e => console.log);
-
-
-	await seeds(require('./models/ProductType.js'), [
-		{type: 'Unico'},
-		{type: 'Combo'}
-	]).then(e => console.log);
-
-	await seeds(require('./models/Tax.js'), [
-		{
-			name: "Exento",
-			percentage: 0
-		},
-	]).then(e => console.log);
-
-	await seeds(require('./models/InvoiceState.js'), [
-		{state: 'Pendiente'},
-		{state: 'Generada'},
-		{state: 'Aprobada'},
-	]).then(e => console.log);
-
+	*/
 })();
-
-
-
-
 
 module.exports = sequelize;
 
