6b47f9d310922e745a1682bfba5e77b06650d918
Jesus Rogliero <jesmirou@gmail.com>
2023-07-02 11:05:23 -0400

    rutas de la interfaz

--- src/public/routes.js
+++ src/public/routes.js
@@ -2,7 +2,10 @@
 
 let routes = [
     {path: "/", component: () => import("./components/Home.js") },
-
+    {path: "/products", component: () => import("./components/Products.js") },
+    {path: "/sales", component: () => import("./components/Sales.js") },
+    {path: "/payments", component: () => import("./components/Payments.js") },
+    {path: "/new_sale", component: () => import("./components/NewSale.js") },
 ];
 
 // exportando rutas designadas
