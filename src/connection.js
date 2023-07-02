679e9778f0c9ba0f614ae6ce910860f54894f77a
Jesus Rogliero <jesmirou@gmail.com>
2023-07-02 11:02:54 -0400

    seed de estados de venta

--- src/connection.js
+++ src/connection.js
@@ -13,8 +13,6 @@ const sequelize = new Sequelize({
 
 const sequelize = new Sequelize('sqlite::memory:', { logging: true });
 
-
-/*
 const seeds = async function (Model, data) {
 	try {
 		for (let i = 0; i < data.length; i++) {
@@ -22,24 +20,21 @@ const seeds = async function (Model, data) {
 				where: data[i]
 			});
 		}
-
 	} catch (error) {
 		console.log(error);
 	}
-
 };
-*/
+
 
 (async () => {
 	await sequelize.sync();
 
-	/*
-	await seeds(require('./models/PurchaseOrderState.js'), [
-	{state: 'Pendiente'},
-	{state: 'Generada'},
-	{state: 'Aprobada'},
+	await seeds(require('./models/SaleState.js'), [
+		{ name: 'Generada' },
+		{ name: 'Pendiente' },
+		{ name: 'Despachada' },
 	]).then(e => console.log);
-	*/
+
 })();
 
 module.exports = sequelize;
