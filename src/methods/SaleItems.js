d348fd4cd2aa0d0757c8de5cc23694fd50ec06eb
Jesus Rogliero <jesmirou@gmail.com>
2023-07-08 00:54:54 -0400

    metodo para despachar los productos

--- src/methods/SaleItems.js
+++ src/methods/SaleItems.js
@@ -25,7 +25,8 @@ const SaleItems = {
 					[sequelize.literal("units || ' UNID'"), 'units'],
 					[sequelize.literal("total_bs || ' BsS'"), 'total_bs'],
 					[sequelize.literal("total_dolar || ' $'"), 'total_dolar'],
-
+					[sequelize.literal("pending_dispatch || ' UNID'"), 'pending_dispatch'],
+					[sequelize.literal("dispatched || ' UNID'"), 'dispatched']
 				],
 				include: [
 					{
@@ -55,12 +56,15 @@ const SaleItems = {
 	'create-item': async function (params) {
 
 		try {
+			if (empty(params.quantity)) throw new Error('Debes ingresar la cantidad a vender');
+			if (params.quantity < 1) throw new Error('Debes ingresar una cantidad correcta');
 
 			const sale = await Sale.findOne({
 				where: { id: params.sale_id }
 			});
 
 			if (sale.state_id == 2) throw new Error('Esta Venta ya fue procesada');
+			if (sale.state_id == 3) throw new Error('Esta Venta ya fue Despachada');
 
 			const product = await Product.findOne({
 				where: { id: params.product_id }
@@ -68,7 +72,7 @@ const SaleItems = {
 
 			if (empty(product)) throw new Error('El producto no existe');
 
-			let total_dolar = product.price_dolar * params.quantity;
+			let total_dolar = parseFloat( (product.price_dolar * params.quantity).toFixed(2) );
 			let total_bs = product.price_bs * params.quantity;
 
 			let item = await SaleItem.create({
@@ -77,16 +81,19 @@ const SaleItems = {
 				quantity: params.quantity,
 				total_bs: total_bs,
 				total_dolar: total_dolar,
-				caps: product.cap * params.quantity,
+				caps: product.cap * (params.quantity * product.quantity),
 				liters: product.liters * params.quantity,
-				units: params.quantity * product.quantity
+				units: params.quantity * product.quantity,
+				pending_dispatch: params.quantity * product.quantity
 			});
 
 			sale.total_dolar = sale.total_dolar + item.total_dolar;
+			sale.total_dolar = sale.total_dolar.toFixed(2);
 			sale.total_bs = sale.total_bs + item.total_bs;
 			sale.total_units = sale.total_units + item.units;
-			sale.total_caps = sale.total_caps + product.cap;
+			sale.total_caps = sale.total_caps + item.caps;
 			sale.total_liters = sale.total_liters + item.liters;
+			sale.pending_dispatch = sale.pending_dispatch + item.pending_dispatch;
 
 			await sale.save();
 			return { message: "Agregado con exito", code: 1 };
@@ -126,6 +133,49 @@ const SaleItems = {
 
 
 	/**
+	 * funcion que ajusta los productos despachados
+	 * 
+	 * @param {*} params 
+	 * @returns message
+	 */
+	'dispatch-item': async function (params) {
+		try {
+
+			const item = await SaleItem.findByPk(params.id);
+
+			if (empty(item)) throw new Error("Este producto no existe");
+
+			const sale = await Sale.findByPk(item.sale_id);
+
+			if (sale.state_id == 3) throw new Error('Esta venta ya fue Despachada');
+
+			if (params.dispatch < 1) throw new Error('Debe introducir la cantidad a despachar correcta');
+			params.dispatch = parseInt(params.dispatch);
+
+			if (params.dispatch > item.pending_dispatch) throw new Error('La cantidad que introdujo es mayor a la cantidad pendiente por despachar')
+
+			item.dispatched = item.dispatched + params.dispatch;
+			item.pending_dispatch = item.pending_dispatch - params.dispatch;
+
+			sale.pending_dispatch = sale.pending_dispatch - params.dispatch;
+			sale.total_dispatched = sale.total_dispatched + params.dispatch;
+
+			if (sale.total_units == sale.total_dispatched && sale.state_id === 2) {
+				sale.state_id = 3;
+			}
+
+			await item.save();
+			await sale.save();
+
+			return { message: "Actualizado Correctamente", code: 1 };
+
+		} catch (error) {
+			log.error(error);
+			return { message: error.message, code: 0 };
+		}
+	},
+
+	/**
 	 * funcion que elimina un recurso
 	 * 
 	 * @param {*} params 
@@ -141,6 +191,7 @@ const SaleItems = {
 			const sale = await Sale.findByPk(item.sale_id);
 
 			if (sale.state_id == 2) throw new Error('Esta Venta ya fue procesada');
+			if (sale.state_id == 3) throw new Error('Esta Venta ya fue Despachada');
 
 			// actualizo la orden
 			sale.total_dolar = sale.total_dolar - item.total_dolar;
@@ -148,9 +199,11 @@ const SaleItems = {
 			sale.total_units = sale.total_units - item.units;
 			sale.total_liters = sale.total_liters - item.liters;
 			sale.total_caps = sale.total_caps - item.caps;
+			sale.pending_dispatch = sale.pending_dispatch - item.pending_dispatch;
+			sale.total_dispatched = sale.total_dispatched - item.dispatched;
 
-			item.destroy();
-			sale.save();
+			await item.destroy();
+			await sale.save();
 
 			return { message: "Eliminado Correctamente", code: 1 };
 
