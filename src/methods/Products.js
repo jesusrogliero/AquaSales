a252adf6f9943b3b2627b0f6a32e879b459792ac
Jesus Rogliero <jesmirou@gmail.com>
2023-07-08 00:54:34 -0400

    controlador de productos finalizado

--- src/methods/Products.js
+++ src/methods/Products.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const sequelize = require('sequelize');
 const Product = require('../models/Product.js');
 const empty = require('../helpers/empty.js');
 const log = require('electron-log');
@@ -14,7 +15,16 @@ const Products = {
      */
     'index-products': async function () {
         try {
-            return await Product.findAll({ raw: true });
+            return await Product.findAll({
+                attributes: [
+					'id', 'name', 'createdAt', 'updatedAt',
+					[sequelize.literal("liters || ' Lt'"), 'liters'],
+					[sequelize.literal("quantity || ' UNID'"), 'quantity'],
+                    [sequelize.literal("cap || ' UNID'"), 'cap'],
+					[sequelize.literal("price_bs || ' BsS'"), 'price_bs'],
+					[sequelize.literal("price_dolar || ' $'"), 'price_dolar'],
+				],
+                raw: true });
         } catch (error) {
             log.error(error.message);
             return { message: error.message, code: 0 };
@@ -32,19 +42,30 @@ const Products = {
     'create-product': async function (params) {
         try {
 
-            if( params.cap > params.quantity) throw new Error('No puedes ingresar mas tapas que porductos');
+            if (params.cap > params.quantity) throw new Error('No puedes ingresar mas tapas que porductos');
 
             let exchange_rate = await bcv.bcvDolar();
-            let price_dolar = params.price_bs / exchange_rate._dolar;
-            price_dolar = price_dolar.toFixed(2);
+            let price_bs = 0
+            let price_dolar = 0;
 
-            const new_product = await Product.create({
+            if(params.is_dolar) {
+                price_dolar = params.price;
+                price_bs = params.price * exchange_rate._dolar;
+            }else {
+                price_dolar = parseFloat(params.price / exchange_rate._dolar);
+                price_dolar = parseFloat(price_dolar.toFixed(3));
+
+                price_bs = params.price;
+            }
+           
+            await Product.create({
                 name: params.name,
                 quantity: params.quantity,
                 liters: params.liters,
                 price_dolar: price_dolar,
-                price_bs: params.price_bs,
-                cap: params.cap
+                price_bs: price_bs,
+                cap: params.cap,
+                is_dolar: params.is_dolar
             });
 
             return { message: "Agregado con exito", code: 1 };
@@ -69,25 +90,31 @@ const Products = {
      * @param {Json} params 
      * @returns message
      */
-       'ajust-products': async function (params) {
+    'ajust-products': async function () {
         try {
 
-            let products =  await Product.findAll({ raw: true });
+            let products = await Product.findAll({ raw: true });
 
-            if(products.length == 0) {
+            if (products.length == 0) {
                 throw new Error('No hay productos registrados');
             }
 
             let exchange_rate = await bcv.bcvDolar();
-            
+            let price_bs = 0;
+            let price_dolar = 0;
+
             products.forEach(async product => {
-                let price_dolar = product.price_bs / exchange_rate._dolar;
-                price_dolar = price_dolar.toFixed(2);
+
+                if(product.is_dolar) {
+                    price_bs = product.price_dolar * exchange_rate._dolar;
+                }else {
+                    price_dolar = parseFloat(product.price_bs / exchange_rate._dolar);
+                    price_dolar = price_dolar.toFixed(3);
+                }
                
-                product.price_dolar = price_dolar;
                 await product.save();
             });
-           
+
             return { message: "Precios ajustados correctamente", code: 1 };
         } catch (error) {
 
@@ -140,11 +167,11 @@ const Products = {
             if (empty(params.liters)) throw new Error("Los litros del producto es obligatorio");
             if (empty(params.price_dolar)) throw new Error("El precio en Dolares es obligatorio");
             if (empty(params.price_bs)) throw new Error("El precio en Bs es obligatorio");
-           
-            if(params.quantity < 0 ) throw new Error('La cantidad del producto no es correcta');
-            if(params.liters < 0 ) throw new Error('La cantidad de litros no es correcta');
-            if(params.price_bs < 0 ) throw new Error('El precio en bolivares no es correcto');
-            if(params.price_dolar < 0 ) throw new Error('El precio es dolares no es correcto');
+
+            if (params.quantity < 0) throw new Error('La cantidad del producto no es correcta');
+            if (params.liters < 0) throw new Error('La cantidad de litros no es correcta');
+            if (params.price_bs < 0) throw new Error('El precio en bolivares no es correcto');
+            if (params.price_dolar < 0) throw new Error('El precio es dolares no es correcto');
             if (params.cap < 0) throw new Error('La cantidad de tapas es incorrecta');
 
             let product = await Product.findByPk(params.id);
@@ -153,7 +180,7 @@ const Products = {
 
             product.name = params.name;
             product.quantity =
-            product.liters = params.liters;
+                product.liters = params.liters;
             product.price_bs = params.price_bs;
             product.price_dolar = params.price_dolar;
             product.cap = params.cap;
