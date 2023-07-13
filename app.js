fc467952e8c468b7d7bf96952c9a3b5df6b64dea
Jesus Rogliero <jesmirou@gmail.com>
2023-07-12 23:06:11 -0400

    copia de seguridad al correo

--- app.js
+++ app.js
@@ -3,13 +3,50 @@ const { loadMethods } = require('./methods');
 const dirs = require('./dirs');
 const path = require('path');
 const appdata = require('appdata-path');
-const log = require('electron-log').transports.file.resolvePath = () => path.join( appdata('sbms'), 'sbms-log.log');
+const log = require('electron-log').transports.file.resolvePath = () => path.join(appdata('sbms'), 'sbms-log.log');
 
 // funcion de inicio de la aplicacion
 const main = function () {
+
+  var nodemailer = require("nodemailer");
+
+  var sender = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'othebestlevel@gmail.com',
+      pass: 'qhqkayvsbxruball'
+    }
+  });
+
+  var mail = {
+    from: "othebestlevel@gmail.com",
+    to: "othebestlevel@gmail.com",
+    subject: "Copia de Seguridad AquaSales",
+    text: "Base de datos del sistema de la embotelladora",
+    attachments: [
+      {
+        filename: 'aqua.data',
+        path: __dirname + '/aqua.data',
+        cid: 'aqua.data' 
+      }
+    ]
+  };
+
+  sender.sendMail(mail, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent successfully: "
+        + info.response);
+    }
+  });
+
+
+
+
   // cargando ventana
   const win = new BrowserWindow({
-    show:false,
+    show: false,
     minWidth: 1110,
     minHeight: 900,
     webPreferences: {
