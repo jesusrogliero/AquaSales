aa73cd1f9560d5b93bf176aeb19390faa841d87d
Jesus Rogliero <jesmirou@gmail.com>
2023-07-20 11:49:51 -0400

    tabla donde se guarda la tasa de cambio

---
+++ src/models/Exchange.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../connection');
+
+const Exchange = sequelize.define('exchange', {
+
+	id: {
+		type: DataTypes.INTEGER,
+		autoIncrement: true,
+		allowNull: false,
+		primaryKey: true
+	},
+
+	bcv: {
+		type: DataTypes.DECIMAL,
+		allowNull: false,
+		defaultValue: 0
+	},
+	
+	updatedAt: {
+		type: DataTypes.DATEONLY,
+		allowNull: false
+	}
+});
+module.exports = Exchange;
