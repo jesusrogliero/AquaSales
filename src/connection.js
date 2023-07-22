6d197014e6246972530f27573e5017868adb1b07
Jesus Rogliero <jesmirou@gmail.com>
2023-07-22 13:23:37 -0400

    la bd se guarda en la carpeta raiz

--- src/connection.js
+++ src/connection.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require('sequelize');
 const appdata = require('appdata-path');
 const path = require('path');
+const hash = require('./helpers/hash.js');
 
 
 /*
@@ -11,7 +12,14 @@ const sequelize = new Sequelize({
 });
 */
 
-const sequelize = new Sequelize('sqlite::memory:', { logging: true });
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'aqua.data',
+  logging: false
+});
+
+
+//const sequelize = new Sequelize('sqlite::memory:', { logging: true });
 
 const seeds = async function (Model, data) {
 	try {
@@ -30,10 +38,41 @@ const seeds = async function (Model, data) {
 	await sequelize.sync();
 
 	await seeds(require('./models/SaleState.js'), [
-		{ name: 'Generada' },
 		{ name: 'Pendiente' },
+		{ name: 'Pendiente por Despachar' },
 		{ name: 'Despachada' },
 	]).then(e => console.log);
+
+	await seeds(require('./models/Admin.js'), [
+		{ 
+			name: 'Leonardo',
+			password: await hash.hash('Leo2022', 10)
+		},
+	]).then(e => console.log);
+
+	let Exchange = require('./models/Exchange.js');
+	let moment = require('moment');
+	let BCV = require('bcv-divisas');
+
+	let today = moment().format("YYYY-MM-DD");
+
+	let exchange = await Exchange.findByPk(1);
+
+	if(exchange == null) {
+		let bcv = await BCV.bcvDolar();
+		bcv =  parseFloat(bcv._dolar);
+
+		exchange = new Exchange();
+		exchange.bcv = bcv;
+		await exchange.save();
+	}
+	else if(exchange.updatedAt != today) {
+		let bcv = await BCV.bcvDolar();
+		bcv =  parseFloat(bcv._dolar);
+
+		exchange.bcv = bcv;
+		await exchange.save();
+	}
 
 })();
 
