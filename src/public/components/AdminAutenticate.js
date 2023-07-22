f52504c47bb6e7c4b1013af1b82a0f476c4acbf2
Jesus Rogliero <jesmirou@gmail.com>
2023-07-22 10:20:18 -0400

    modulo de autenticacion del admin

--- src/public/components/AdminAutenticate.js
+++ src/public/components/AdminAutenticate.js
@@ -12,8 +12,12 @@ var AdminAutenticate = Vue.component('admin-autenticate', {
         };
     },
 
-    mounted(){
-        this.session = localStorage.getItem('session');
+    mounted() {
+        if (localStorage.getItem('session') == 'active') {
+            console.log(this.session);
+            this.session = true;
+        }
+
     },
 
     methods: {
@@ -27,10 +31,9 @@ var AdminAutenticate = Vue.component('admin-autenticate', {
                 }
                 this.admin_name = response.name;
                 this.dialog = true;
-            
+
             } catch (error) {
-                console.log(error);
-                alertApp('error', 'alert', error.message);
+                alertApp({color: "error", icon: "alert", text: error.message});
             }
         },
 
@@ -44,11 +47,28 @@ var AdminAutenticate = Vue.component('admin-autenticate', {
                 console.log(response);
                 this.admin_name = null;
                 this.dialog = null;
-                window.localStorage.setItem('session', response);
-           
+                this.session = response;
+                window.localStorage.setItem('session', 'active');
+
             } catch (error) {
                 console.log(error);
-                alertApp('error', 'alert', error.message);
+                alertApp({color: "error", icon: "alert", text: error.message});
+            } finally {
+                this.password = null;
+            }
+        },
+
+        async logout() {
+            try {
+                let response = await execute('logout');
+
+                if (response.code === 0) {
+                    throw new Error(response.message)
+                }
+                window.localStorage.setItem('session', null);
+                this.session = null;
+            } catch (error) {
+                alertApp({color: "error", icon: "alert", text: error.message});
             }
         },
 
@@ -58,45 +78,47 @@ var AdminAutenticate = Vue.component('admin-autenticate', {
     },
 
     template: `
-		<div>
 
-        <v-btn icon @click="openDialog">
-            <v-icon size="30" v-if="session == true">mdi-account-lock-open-outline</v-icon>
-            <v-icon size="30" v-else >mdi-account-lock-outline</v-icon>
+    <div>
+        <v-btn v-if="session != true" icon @click="openDialog">
+            <v-icon size="30">mdi-account-lock-outline</v-icon>
         </v-btn>
 
-            <dialog-base :active="dialog">
-                <template v-slot:dialog-title>
-                    <span class="title">Hola {{admin_name}}!!, Ingresa tu contraseña</span>
-                </template>
+        <v-btn v-else icon @click="logout">
+            <v-icon size="30" >mdi-account-lock-open-outline</v-icon>
+        </v-btn>
 
-                <template v-slot:dialog-content>
+        <dialog-base :active="dialog">
+            <template v-slot:dialog-title>
+                <span class="title">Hola {{admin_name}}!!, Ingresa tu contraseña</span>
+            </template>
 
-                    <v-form>
-                        <v-row>
-                            <v-col cols="12">
-                                <v-text-field v-model="password" type="password" label="Contraseña"
-                                    placeholder="Ingresa tu contraseña de administrador"></v-text-field>
-                            </v-col>
-                        </v-row>
-                    </v-form>
-                </template>
+            <template v-slot:dialog-content>
 
-                <template v-slot:dialog-actions>
-                    <v-btn color="transparent" text style="color:#2c823c !important;" class="mr-4"
-                        @click="validate">
-                        <span>Guardar</span>
-                        <v-icon>mdi-check</v-icon>
-                    </v-btn>
+                <v-form>
+                    <v-row>
+                        <v-col cols="12">
+                            <v-text-field v-model="password" type="password" label="Contraseña"
+                                placeholder="Ingresa tu contraseña de administrador"></v-text-field>
+                        </v-col>
+                    </v-row>
+                </v-form>
+            </template>
 
-                    <v-btn @click="closeDialog" color="transparent" text style="color: #f44336 !important;">
-                        <span>Cancelar</span>
-                        <v-icon>mdi-close</v-icon>
-                    </v-btn>
-                </template>
-            </dialog-base>
-			
-		</div>
+            <template v-slot:dialog-actions>
+                <v-btn color="transparent" text style="color:#2c823c !important;" class="mr-4"
+                    @click="validate">
+                    <span>Guardar</span>
+                    <v-icon>mdi-check</v-icon>
+                </v-btn>
+
+                <v-btn @click="closeDialog" color="transparent" text style="color: #f44336 !important;">
+                    <span>Cancelar</span>
+                    <v-icon>mdi-close</v-icon>
+                </v-btn>
+            </template>
+        </dialog-base>        
+    </div>
 	`
 });
 
