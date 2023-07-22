43b837a67c6759b7c3be0ee847836db4146d5b05
Jesus Rogliero <jesmirou@gmail.com>
2023-07-22 10:18:04 -0400

    copia de seguridad por mail identacion del texto

--- app.js
+++ app.js
@@ -8,68 +8,65 @@ const log = require('electron-log').transports.file.resolvePath = () => path.joi
 // funcion de inicio de la aplicacion
 const main = function () {
 
-  var nodemailer = require("nodemailer");
+	const nodemailer = require("nodemailer");
 
-  var sender = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'othebestlevel@gmail.com',
-      pass: 'qhqkayvsbxruball'
-    }
-  });
+	let sender = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: 'othebestlevel@gmail.com',
+			pass: 'qhqkayvsbxruball'
+		}
+	});
 
-  var mail = {
-    from: "othebestlevel@gmail.com",
-    to: "othebestlevel@gmail.com",
-    subject: "Copia de Seguridad AquaSales",
-    text: "Base de datos del sistema de la embotelladora",
-    attachments: [
-      {
-        filename: 'aqua.data',
-        path: __dirname + '/aqua.data',
-        cid: 'aqua.data' 
-      }
-    ]
-  };
+	let mail = {
+		from: "othebestlevel@gmail.com",
+		to: "othebestlevel@gmail.com",
+		subject: "Copia de Seguridad AquaSales",
+		text: "Base de datos del sistema de la embotelladora",
+		attachments: [
+			{
+				filename: 'aqua.data',
+				path: __dirname + '/aqua.data',
+				cid: 'aqua.data'
+			}
+		]
+	};
 
-  sender.sendMail(mail, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent successfully: "
-        + info.response);
-    }
-  });
+	sender.sendMail(mail, function (error, info) {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log("Email sent successfully: "
+				+ info.response);
+		}
+	});
 
+	// cargando ventana
+	const win = new BrowserWindow({
+		show: false,
+		minWidth: 1110,
+		minHeight: 900,
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: false,
+		},
+	});
 
-
-
-  // cargando ventana
-  const win = new BrowserWindow({
-    show: false,
-    minWidth: 1110,
-    minHeight: 900,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-    },
-  });
-
-  win.maximize();
-  win.setMenuBarVisibility(false)
-  win.loadFile(dirs.public + 'index.html');
+	win.maximize();
+	win.setMenuBarVisibility(false)
+	win.loadFile(dirs.public + 'index.html');
 
 
-  // cargando el listado de archivos
-  const fs = require('fs');
-  fs.readdir(dirs.methods, (error, files) => {
-    if (!error) loadMethods(files);
-    else console.error(error);
-  });
+	// cargando el listado de archivos
+	const fs = require('fs');
+	fs.readdir(dirs.methods, (error, files) => {
+		if (!error) loadMethods(files);
+		else console.error(error);
+	});
 
-  win.once('ready-to-show', () => {
-    win.show();
-  });
+	win.once('ready-to-show', () => {
+		win.show();
+	});
 
 };
 
