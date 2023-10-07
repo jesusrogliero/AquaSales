59241b312033d1f4a76114ae67002a85fdda663e
Jesus Rogliero <jesmirou@gmail.com>
2023-10-07 15:58:10 -0400

    metodo que genera el reporte de resumen de ventas

--- src/methods/SumariesReports.js
+++ src/methods/SumariesReports.js
@@ -7,7 +7,7 @@ const sequelize = require("../connection.js");
 const { Op } = require('sequelize');
 const moment = require('moment');
 const reportErrors = require('../helpers/reportErrors.js');
-const empty = require('../helpers/empty.js');
+const pdf = require('../helpers/ExportPdf.js');
 
 const Sumaries = {
 
@@ -18,18 +18,6 @@ const Sumaries = {
     */
     'sumary-range-date': async function (params) {
         try {
-            /*
-            let date = null;
-
-            if (empty(param)) 
-                date = moment();
-            else 
-                date = moment(param);
-            
-            const start = date.startOf('week').format("YYYY-MM-DD");
-            const end = date.endOf('week').format("YYYY-MM-DD");
-            */
-
             let option = {
                 attributes: [
                     sequelize.col('payment.createdAt'),
@@ -46,7 +34,6 @@ const Sumaries = {
                         model: Sale,
                         required: true,
                         attributes: [],
-                        /* where: { state_id: 3}. */
                     }
                 ],
                 where: {
@@ -65,7 +52,6 @@ const Sumaries = {
                 }
             }
 
-
             let data = await Payment.findAll(option);
             return data;
 
@@ -75,6 +61,83 @@ const Sumaries = {
             return { message: error.message, code: 0 };
         }
     },
+
+    /**
+     * Metricas 
+     * 
+     * @returns {json} pago
+    */
+    'sumary-report': async function (days) {
+        let initDate = moment();
+        let finalDate = moment().subtract(days, 'days');
+
+        try {
+            let option = {
+                attributes: [
+                    sequelize.col('payment.createdAt'),
+                    [sequelize.literal("sum(mobile_payment) || ' BsS' "), 'mobile_payment'],
+                    [sequelize.literal("sum(cash_dollar) || ' $' "), 'cash_dollar'],
+                    [sequelize.literal("sum(cash_bolivares) || ' BsS' "), 'cash_bolivares'],
+                    [sequelize.literal("sum(total_units) || ' UNID' "), 'sales_units'],
+                    [sequelize.literal("sum(total_liters) || ' Lts' "), 'liters_consumption'],
+                    [sequelize.literal("sum(total_caps)  || ' UNID' "), 'total_caps'],
+                ],
+                group: [sequelize.col('payment.createdAt')],
+                include: [
+                    {
+                        model: Sale,
+                        required: true,
+                        attributes: [],
+                    }
+                ],
+                where: {
+                    createdAt: {
+                        [Op.between]: [finalDate.format("YYYY-MM-DD"), initDate.format("YYYY-MM-DD")]
+                    }
+                },
+                raw: true
+            };
+
+            let sales = await Payment.findAll(option);
+
+            let totals_sales = await Payment.findAll({
+                attributes: [
+                    [sequelize.literal("( sum(mobile_payment) + sum(cash_bolivares) )  || ' BsS' "), 'total_sales_bs'],
+                    [sequelize.literal("sum(cash_dollar) || ' $' "), 'total_sales_dolar'],
+                    [sequelize.literal("sum(total_units) || ' UNID' "), 'total_sales_units'],
+                    [sequelize.literal("sum(total_liters)  || ' Lts' "), 'total_sales_liters'],
+                    [sequelize.literal("sum(total_caps)  || ' UNID' "), 'total_sales_caps'],
+                ],
+                include: [
+                    {
+                        model: Sale,
+                        required: true,
+                        attributes: [],
+                    }
+                ],
+                where: {
+                    createdAt: {
+                        [Op.between]: [finalDate.format("YYYY-MM-DD"), initDate.format("YYYY-MM-DD")]
+                    }
+                },
+                raw: true
+            });
+
+            pdf({
+                title: `Resumen de Venta`,
+                sales: sales,
+                totals_sales: totals_sales
+            }, 'sumarySales.html');
+
+            return { message: 'Reporte Creado Correctamente', code: 1 };
+
+        } catch (error) {
+            log.error(error.message);
+            reportErrors(error);
+            return { message: error.message, code: 0 };
+        }
+
+    }
 
 };
 
